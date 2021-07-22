910497c3178170911949189703a4588b21b10765
Brad Treloar <brad.treloar@pm.me>
2021-07-22 18:19:31 +0930

    split ssh config into own model

--- manager/sites.py
+++ manager/sites.py
@@ -1,41 +1,39 @@
 
 
-from sqlalchemy.sql.schema import Column
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import Integer, String
 
 from manager.database import Model
 
 
 def import_sites(sites, db_session):
+    """Adds or updates each site from the config in the database."""
     for site_host, site_config in sites.items():
         site_config["host"] = site_host
         site_attributes = {
             "host": site_host,
-            "ssh_hostname": site_host,
-            "ssh_port": 22,
         }
         if "app" in site_config.keys():
             site_attributes["app"] = site_config["app"]
-        if "ssh" in site_config.keys():
-            site_ssh_config = site_config["ssh"]
-            if "hostname" in site_ssh_config.keys():
-                site_attributes["ssh_hostname"] = site_ssh_config["hostname"]
-            if "port" in site_ssh_config.keys():
-                site_attributes["ssh_port"] = site_ssh_config["port"]
-            if "user" in site_ssh_config.keys():
-                site_attributes["ssh_user"] = site_ssh_config["user"]
-            if "key" in site_ssh_config.keys():
-                site_attributes["ssh_key_filename"] = site_ssh_config["key"]
         site = db_session.query(Site).filter(
             Site.host == site_host).scalar()
         if not site:
             site = Site(**site_attributes)
             db_session.add(site)
-            print("created", site)
         else:
             for key, value in site_attributes.items():
                 setattr(site, key, value)
-            print("updated", site)
+        ssh_config_attributes = None
+        if "ssh" in site_config.keys():
+            ssh_config_attributes = site_config["ssh"]
+            if "host" not in ssh_config_attributes.keys():
+                ssh_config_attributes["host"] = site_host
+            if not site.ssh_config:
+                site.ssh_config = SiteSSHConfig(**ssh_config_attributes)
+            else:
+                for key, value in ssh_config_attributes.items():
+                    setattr(site.ssh_config, key, value)
         db_session.commit()
 
 
@@ -44,12 +42,32 @@ class Site(Model):
 
     id = Column(Integer, primary_key=True)
     host = Column(String(255), unique=True, nullable=False)
-    ssh_hostname = Column(String(255))
-    ssh_port = Column(Integer, default=22)
-    ssh_user = Column(String(255))
-    ssh_key_filename = Column(String(1023))
     app = Column(String(255))
 
     def __repr__(self):
         return "<Site(site='{}')>".format(
             self.host)
+
+
+class SiteSSHConfig(Model):
+    __tablename__ = "site_ssh_configs"
+
+    id = Column(Integer, primary_key=True)
+    site_id = Column(Integer,
+                     ForeignKey("sites.id"), nullable=False)
+    host = Column(String(255), nullable=False)
+    port = Column(Integer, default=22, nullable=False)
+    user = Column(String(255), nullable=False)
+    key_filename = Column(String(1023))
+
+    site = relationship(Site, back_populates="ssh_config")
+
+    def __repr__(self):
+        return "<SiteSSHConfig(site='{}')>".format(
+            self.site.host)
+
+
+Site.ssh_config = relationship(SiteSSHConfig,
+                               uselist=False,
+                               back_populates="site",
+                               cascade="all, delete")
