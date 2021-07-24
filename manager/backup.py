c96c8484795d245b98e74b7859d6ae76fb043a7b
Brad Treloar <brad.treloar@pm.me>
2021-07-24 23:58:52 +0930

    handle drupal multisite backups

--- manager/backup.py
+++ manager/backup.py
@@ -14,8 +14,10 @@ CLIENTS = {
 }
 
 
-def backup_cms_app(app, site_info, aws_config):
-    temp_dir = "/tmp/wordpress_backups/{}/".format(site_info["site_host"])
+def backup_wordpress_site(args):
+    site_info, aws_config = args
+    app = "wordpress"
+    temp_dir = "/tmp/{}_backups/{}/".format(app, site_info["site_host"])
     # Download backup.
     client = CLIENTS[app](
         site_info["ssh_config"], site_info["backup_bucket"])
@@ -40,11 +42,32 @@ def backup_cms_app(app, site_info, aws_config):
     shutil.rmtree(archive_dirpath)
 
 
-def backup_wordpress_site(args):
-    site_info, aws_config = args
-    backup_cms_app("wordpress", site_info, aws_config)
-
-
 def backup_drupal_site(args):
     site_info, aws_config = args
-    backup_cms_app("drupal", site_info, aws_config)
+    app = "drupal"
+    temp_dir = "/tmp/{}_backups/{}/".format(app, site_info["site_host"])
+    client = CLIENTS[app](
+        site_info["ssh_config"], site_info["backup_bucket"])
+    client.start_webauth_session()
+    # Get site names
+    site_names = client.site_names()
+    # Download backups.
+    for site_name in site_names:
+        client.export_database(site_name, temp_dir)
+        client.download_site_files(site_name, temp_dir)
+    # Create backup tarball.
+    archive_dirpath = "/tmp/archives/{}".format(site_info["site_host"])
+    archive_filepath = "{}/{}_backup_{}.tar.gz".format(
+        archive_dirpath,
+        app,
+        datetime.now().isoformat()
+    )
+    make_backup_archive(archive_filepath, temp_dir)
+    # Upload tarball to S3.
+    s3_backup_bucket_client = S3BackupBucketClient(
+        aws_config, site_info["backup_bucket"])
+    s3_backup_bucket_client.create()
+    s3_backup_bucket_client.upload_archive(archive_filepath)
+    # Cleanup temporary files.
+    shutil.rmtree(temp_dir)
+    shutil.rmtree(archive_dirpath)
--- manager/remote/drupal.py
+++ manager/remote/drupal.py
@@ -23,20 +23,38 @@ class DrupalClient:
             "cd drupal && vendor/bin/drupal site:status")
         return status.split('\n')[1].strip().split(" ")[-1]
 
-    def export_database(self, filepath):
+    def site_names(self):
+        sites_filepath = "drupal/web/sites/sites.php"
+        if not exists(self.remote_client, sites_filepath):
+            return ['default']
+        lines = self.remote_client.exec_command(
+            "cat " + sites_filepath).split('\n')
+        site_names = []
+        for line in lines:
+            pattern = r"""\$sites\[['"]{1}[^'"]{1,}['"]{1}\]\s{0,1}=\s{0,1}['"]{1}([^'"]{1,})['"]{1};"""
+            matches = re.search(pattern, line.strip())
+            if matches:
+                site_name = matches[1]
+                if site_name not in site_names:
+                    site_names.append(site_name)
+        return site_names
+
+    def export_database(self, site_name, dirpath):
+        filepath = dirpath + "/data/{}/drupal.sql".format(site_name)
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
         home_path = self.remote_client.exec_command("pwd")
         self.remote_client.exec_command(
             "mkdir -p {}/tmp".format(home_path))
         temporary_database_filepath = home_path + "/tmp/drupal.sql"
+        alias = "@self" if site_name == "default" else "@" + site_name
         self.remote_client.exec_command(
             "cd drupal && vendor/bin/drupal database:dump --no-interaction --quiet --file {}".format(temporary_database_filepath))
         self.remote_client.download_file(temporary_database_filepath, filepath)
         self.remote_client.exec_command(
             "rm {}".format(temporary_database_filepath))
 
-    def download_site_files(self, dirpath):
-        files_path = "web/sites/default/files"
+    def download_site_files(self, site_name, dirpath):
+        files_path = "web/sites/{}/files".format(site_name)
         os.makedirs(os.path.join(dirpath, files_path), exist_ok=True)
         for filename in ls(self.remote_client, "drupal/" + files_path):
             if filename not in IGNORED_FILES:
