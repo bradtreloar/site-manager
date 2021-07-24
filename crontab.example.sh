224edeb0436142d07177e071b69ce679c506667d
Brad Treloar <brad.treloar@pm.me>
2021-07-24 23:15:21 +0930

    update crontab example

--- crontab.example.sh
+++ crontab.example.sh
@@ -1,19 +1,12 @@
 
 SHELL=/bin/bash
-APP_ROOT=/app
-RUN="/usr/bin/python3 $APP_ROOT/run.py"
 
 # Run status monitor every five minutes
-*/5 * * * *      $RUN update_https_status
-*/5 * * * *      $RUN update_ssh_status
+*/5 * * * *      cd /home/ubuntu/site-manager && /usr/bin/python3 run.py update_https_status
 
 # Send status report to sysadmin every Monday at 9:00
-00 09 * * mon    $RUN send_status_report
+00 09 * * mon    cd /home/ubuntu/site-manager && /usr/bin/python3 run.py send_status_report
 
 # Backup websites every Sunday
-00 09 * * sun    $RUN backup_wordpress
-00 10 * * sun    $RUN backup_drupal
-00 11 * * sun    $RUN backup_mysql
-
-# Run DNS monitor daily
-00 12 * * *      $RUN dns_monitor
+00 09 * * sun    cd /home/ubuntu/site-manager && /usr/bin/python3 run.py backup_wordpress
+00 10 * * sun    cd /home/ubuntu/site-manager && /usr/bin/python3 run.py backup_drupal
