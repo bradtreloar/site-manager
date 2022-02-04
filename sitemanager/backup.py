36e60b0c16c96b70228e438100efa8196f7b066a
Brad Treloar <brad.treloar@pm.me>
2022-02-04 22:54:37 +1030

    add typings, cleanup

--- sitemanager/backup.py
+++ sitemanager/backup.py
@@ -5,6 +5,8 @@ import shutil
 
 from sitemanager.archive import make_gzipped_tarball
 from sitemanager.aws import S3BackupBucketClient
+from sitemanager.config import AWSConfig, BackupConfig
+from sitemanager.models import Site
 from sitemanager.remote import get_remote_client
 from sitemanager.remote.wordpress import WordpressClient
 from sitemanager.remote.drupal import DrupalClient
@@ -14,7 +16,10 @@ TEMP_BACKUPS_DIR_TEMPLATE = "/tmp/backups/{}"
 TEMP_ARCHIVE_DIR_TEMPLATE = "/tmp/archives/{}"
 
 
-def backup_app(site, backup_config, aws_config=None):
+def backup_app(
+        site: Site,
+        backup_config: BackupConfig,
+        aws_config: AWSConfig = None):
     """
     Downloads the files and database for a Wordpress site and uploads them to 
     an S3 bucket as an archive file.
@@ -22,11 +27,11 @@ def backup_app(site, backup_config, aws_config=None):
     # Temporary directory for storing backups.
     backups_dirpath = TEMP_BACKUPS_DIR_TEMPLATE.format(site.host)
 
-    # Initialise app client.
+    # Initialise app client, start webauth session.
     app_client = get_remote_client(site.app)(site.ssh_config.to_dict())
     app_client.start_webauth_session()
 
-    # Download backup.
+    # Download files to be backed up.
     app_client.export_databases(backups_dirpath)
     app_client.download_generated_files(backups_dirpath)
 
@@ -38,7 +43,7 @@ def backup_app(site, backup_config, aws_config=None):
     archive_filepath = f"{archive_dirpath}/{archive_filename}"
     make_gzipped_tarball(archive_filepath, backups_dirpath)
 
-    if backup_config["filesystem"].lower() == "s3":
+    if is_s3_backup(backup_config):
         # Upload tarball to S3.
         bucket_name = backup_config["bucket"].format(site_host=site.host)
         s3_backup_bucket_client = S3BackupBucketClient(aws_config, bucket_name)
@@ -53,3 +58,7 @@ def backup_app(site, backup_config, aws_config=None):
     # Delete temporary files.
     shutil.rmtree(backups_dirpath)
     shutil.rmtree(archive_dirpath)
+
+
+def is_s3_backup(backup_config: BackupConfig):
+    return backup_config["filesystem"].lower() == "s3"
