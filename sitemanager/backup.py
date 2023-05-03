f2f139e4889793ffdb494dff71135305c1d7ba20
Brad Treloar <brad.treloar@pm.me>
2023-05-03 19:36:14 +0930

    Print progress

--- sitemanager/backup.py
+++ sitemanager/backup.py
@@ -24,17 +24,21 @@ def backup_app(
     Downloads the files and database for a Wordpress site and uploads them to 
     an S3 bucket as an archive file.
     """
+    print(site.host)
+
     # Temporary directory for storing backups.
     backups_dirpath = TEMP_BACKUPS_DIR_TEMPLATE.format(site.host)
 
     # Initialise app client.
     app_client = get_remote_client(site.app)(site.ssh_config.to_dict())
 
-    # Download files to be backed up.
+    print("- Exporting database.")
     app_client.export_databases(backups_dirpath)
+
+    print("- Downloading site files.")
     app_client.download_generated_files(backups_dirpath)
 
-    # Create backup tarball.
+    print("- Creating tarball.")
     archive_dirpath = TEMP_ARCHIVE_DIR_TEMPLATE.format(site.host)
     archive_filename = (
         f"{site.app}_backup_{datetime.now(timezone.utc).isoformat(timespec='seconds')}"
@@ -43,13 +47,13 @@ def backup_app(
     make_gzipped_tarball(archive_filepath, backups_dirpath)
 
     if is_s3_backup(backup_config):
-        # Upload tarball to S3.
+        print("- Uploading to S3.")
         bucket_name = backup_config["bucket"].format(site_host=site.host)
         s3_backup_bucket_client = S3Client(aws_config, bucket_name)
         s3_backup_bucket_client.create_bucket()
         s3_backup_bucket_client.upload_archive_to_bucket(archive_filepath)
     else:
-        # Make sure destination directory exists then copy archive.
+        print("- Copying to backup directory.")
         dirpath = backup_config["path"]
         os.makedirs(dirpath, exist_ok=True)
         shutil.copy(archive_filepath, os.path.join(dirpath, archive_filename))
